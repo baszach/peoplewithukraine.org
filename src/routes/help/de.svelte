fc3dc034943d64b628b47d3af21b14c67fcab8b2
vasilios <v.zachopoulos@yahoo.de>
2022-03-06 14:01:14 +0200

    added app.css & layout reset for help route & Countries component

--- src/routes/help/de.svelte
+++ src/routes/help/de.svelte
@@ -1,22 +1,3 @@
-<div style="display: flex; justify-content: center">
-    <div class="country_label"><a href="/help/pl">Польща</a></div>
-    <div class="country_label"><a href="/help/de">Німеччина</a></div>
-    <div class="country_label"><a href="/help/cz">Чехія</a></div>
-</div>
-<style>
-    .country_label{
-        padding: 1em 2em;
-        margin: 1em;
-        background-color: #afe2ff;
-        border-radius: 50px;
-    }
-
-    .country_label:hover{
-        background-color: #ccebfa;
-    }
-</style>
-<!--FAIL copy paste-->
-
 <h2>Transport</h2>
 <div>
     <h3>Deutsche Bahn (Дойче бан) - німецька залізниця</h3>
