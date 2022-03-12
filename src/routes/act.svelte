db41306e6fccdbfa404e894a657c933bd8bbd27c
Vassilis Zachopoulos <v.zachopoulos@yahoo.de>
2022-03-12 21:16:54 +0100

    feat: basic skeleton of news and act subpages

--- src/routes/act.svelte
+++ src/routes/act.svelte
@@ -1,1 +1,40 @@
-<div>donate, act, demonstrate</div>
+<div>
+    <div>
+        <h1 class="text-3xl font-bold text-accent-gray">Donate</h1>
+        <div>
+            Ukraine military
+            IBAN xxxx xxxx xxxx xxxx
+            ukrainemilitary.ua
+            Xreshyatik 5, Kyiv
+
+            CryptoUkraine
+            cryptoukraine.com.ua
+            (donate cryptocurrency by scanning their QR-Code)
+
+            Humanitarian UA
+            IBAN xxxx xxxx xxxx xxxx
+            humanitua.org
+            info@humanitua.org
+        </div>
+    </div>
+    
+    <div>
+        <h1 class="text-3xl font-bold text-accent-gray">Act</h1>
+        <div>
+            Share verified news:As this war is fought not only with weapons but also with information, it is very important to share information and stop the spreading of fake news. Try to report abusive, hateful or false content on social media platforms like Twitter.
+
+            Join organizations and help groups:There are many different types of groups that help. They may collect food, blankets and other supply for Ukraine. Other groups are helping arriving refugees at airports and train stations of big cities (e.g. Berlin or Krakow).
+
+            Find creative solutions:Many people use the knowledge of their professions to support Ukraine. IT specialists can help with cyberdefense, logistic experts can help with transportation of supply to Ukraine, photographers can take pictures of demonstrations, designers can make supportive art and influencers can call out Russia for their actions and share the truth. There’s no limit to human creativity.
+
+            Reach out to politicians and companies:
+            In the last days many companies have cut ties with Russia or temporarily stopped operating businesses there. It is important that many people raise their voice to hurt Russia’s economy. Write letters, emails or comments to companies and politicians and urge them to reconsider their views (e.g. drawing back from Russia).
+       
+            Demonstrate:
+        </div>
+    </div>  
+
+    <div>
+        <h1 class="text-3xl font-bold text-accent-gray">Companies</h1>
+    </div>
+</div>
