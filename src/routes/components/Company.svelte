922a7f66faf8b2ea32d9611035a2622b4aecc5c6
madhol <imaginehogwarts@gmail.com>
2022-03-21 19:49:22 +0100

    Fixed some stuffff

--- src/routes/components/Company.svelte
+++ src/routes/components/Company.svelte
@@ -9,6 +9,8 @@
 	let flipped = false;
 	let hovering = false;
 
+	let icon_size = killer.pic != null || killer.children != null ? '2' : '1.5';
+
 	function hover() {
 		if (!flipped) hovering = true;
 	}
@@ -35,7 +37,7 @@
 			alt=""
 			loading="lazy"
 			class="company-image px-2"
-			style="width: 100%; opacity: {flipped ? '0%' : '100%'}"
+			style="width: 100%; z-index: {flipped ? '10' : '0'}; opacity: {flipped ? '0%' : '100%'}"
 		/>
 		{#if hovering}
 			<div
@@ -45,56 +47,56 @@
 			/>
 		{/if}
 
-		<div
-			class="p-3 text-left bg-slate-200 grid items-center"
-			style="height: 100%; width: 100%; grid-area: 1/1/1/1; opacity: {flipped ? '100%' : '0%'};"
-		>
-			<div class="">
-				<div class="p-2 m-1 font-bold inline-block bg-white rounded-lg">{killer.name}</div>
-				{#if killer.all_brands != null}
-					<div>
-						<a
-							class="link p-2 m-1 font-bold bg-ukraine-yellow rounded-lg inline-block"
-							target="_blank"
-							href={killer.all_brands}
-							style="z-index: 40">view all brands</a
-						>
-					</div>
-				{/if}
-				<div class="flex justify-between">
+		{#if flipped}
+			<div
+				class="p-3 text-left bg-slate-200 grid items-center"
+				style="height: 100%; width: 100%; grid-area: 1/1/1/1;"
+			>
+				<div class="">
+					<div class="p-2 m-1 font-bold inline-block bg-white rounded-lg">{killer.name}</div>
+					{#if killer.all_brands != null}
+						<div>
+							<a
+								class="link p-2 m-1 font-bold bg-ukraine-yellow rounded-lg inline-block"
+								target="_blank"
+								href={killer.all_brands}>view all brands</a
+							>
+						</div>
+					{/if}
 					<div class="p-2 m-1 bg-ukraine-blue text-white font-bold rounded-lg inline-block">
 						{killer.area}
 					</div>
-					{#if killer.linkedin != null}<a
-							class="link p-1"
-							target="_blank"
-							href={killer.linkedin}
-							style="z-index: 40"><LinkedInComponent /></a
-						>{/if}
-				</div>
-				<div class="flex justify-between">
-					{#if killer.facebook != null}<a
-							class="link p-1"
-							target="_blank"
-							href={killer.facebook}
-							style="z-index: 40;"><FacebookIcon /></a
-						>{/if}
-					{#if killer.twitter != null}<a
-							class="link p-1"
-							target="_blank"
-							href={killer.twitter}
-							style="z-index: 40;"><TwitterIcon /></a
-						>{/if}
-					{#if killer.instagram != null}<a
-							class="link p-1"
-							target="_blank"
-							href={killer.instagram}
-							style="z-index: 40;"><InstagramIcon /></a
-						>{/if}
+					<div class="flex">
+						{#if killer.linkedin != null}<a
+								class="link p-1"
+								target="_blank"
+								href={killer.linkedin}
+								style="z-index: 40"><LinkedInComponent size={icon_size} /></a
+							>{/if}
+						{#if killer.facebook != null}<a
+								class="link p-1"
+								target="_blank"
+								href={killer.facebook}
+								style="z-index: 40;"><FacebookIcon size={icon_size} /></a
+							>{/if}
+						{#if killer.twitter != null}<a
+								class="link p-1"
+								target="_blank"
+								href={killer.twitter}
+								style="z-index: 40;"><TwitterIcon size={icon_size} /></a
+							>{/if}
+						{#if killer.instagram != null}<a
+								class="link p-1"
+								target="_blank"
+								href={killer.instagram}
+								style="z-index: 40;"><InstagramIcon size={icon_size} /></a
+							>{/if}
+					</div>
 				</div>
 			</div>
-		</div>
+		{/if}
 	</div>
+
 	{#if killer.children != null}
 		<div class="place-items-center companies-children-grid bg-slate-200">
 			{#each killer.children as child}
